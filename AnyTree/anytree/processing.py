7de779646304352c0f3d5b8062b35c1336d892fe
Christophe Combelles <ccomb@free.fr>
2013-04-29 23:58:42 +0200

    First POC for mapping application

--- AnyTree/anytree/processing.py
+++ AnyTree/anytree/processing.py
@@ -1,4 +1,9 @@
 import csv
+import logging
+from os.path import dirname, basename, join
+
+logging.basicConfig(level=logging.DEBUG)
+LOG = logging.getLogger(basename(__file__))
 
 
 class CSVProcessor(object):
@@ -8,15 +13,38 @@ class CSVProcessor(object):
     def __init__(self, mapping):
         self.mapping = mapping
 
-    def process(self, in_filename):
-        table = in_filename.rsplit('.', 1)[0]
-        out_columns = self.mapping.out_columns
-        out_filename = table + '.out.csv'
-        with open(in_filename, 'rb') as in_csv, open(out_filename, 'wb') as out_csv:
-            reader = csv.DictReader(in_csv, delimiter=',')
-            writer = csv.DictWriter(out_csv, delimiter=',')
-            for inrow in reader:
-                for column in inrow:
-                    outrow = {}
-                    outrow
-                writer.writerow(outrow)
+    def process(self, src_filename):
+        """ The main processing method
+        """
+        directory = dirname(src_filename)
+        src_table = basename(src_filename).rsplit('.', 1)[0]
+        dst_columns = self.mapping.dst_columns
+        with open(src_filename, 'rb') as src_csv:
+            reader = csv.DictReader(src_csv, delimiter=',')
+            dst_files = {t: open(join(directory, t + '.out.csv'), 'wb') for t in dst_columns}
+            writers = {t: csv.DictWriter(f, dst_columns[t], delimiter=',')
+                       for t, f in dst_files.items()}
+            for writer in writers.values():
+                writer.writeheader()
+            # process each csv line
+            for src_row in reader:
+                dst_rows = {t: {} for t in dst_columns}
+                # process each column
+                for src_column in src_row:
+                    mapping = self.mapping.mapping.get(src_table + '.' + src_column)
+                    if mapping is None:
+                        LOG.warn('No mapping found for column %s', src_table + '.' + src_column)
+                        continue
+                    # we found a mapping, use it
+                    for dst_column, function in mapping.items():
+                        dst_table, dst_column = dst_column.split('.')
+                        if function is None:
+                            # mapping is empty: use identity
+                            dst_rows[src_table][dst_column] = src_row[src_column]
+                        else:
+                            # mapping is a function.
+                            dst_rows[src_table][dst_column] = function(src_row)
+                for table, dst_row in dst_rows.items():
+                    writers[table].writerow(dst_row)
+            for dst_file in dst_files.values():
+                dst_file.close()
