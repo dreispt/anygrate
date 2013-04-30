f4da880953928d66cfab9754e8ace4caa11e0aa7
Christophe Combelles <ccomb@free.fr>
2013-04-30 11:52:56 +0200

    implemented console script

--- AnyTree/anytree/exporting.py
+++ AnyTree/anytree/exporting.py
@@ -5,7 +5,11 @@ import psycopg2
 def export_tables(source_tables, dest_dir, db=None, user='admin', pwd='admin'):
     """ Export the list of tables using postgresql COPY
     """
+    csv_filenames = []
     connection = psycopg2.connect("dbname=%s" % db)
     for table in source_tables:
-        with connection.cursor() as cursor, open(join(dest_dir, table + '.csv'), 'w') as f:
+        filename = join(dest_dir, table + '.csv')
+        with connection.cursor() as cursor, open(filename, 'w') as f:
             cursor.copy_expert("COPY %s TO STDOUT WITH CSV HEADER" % table, f)
+            csv_filenames.append(filename)
+    return csv_filenames
