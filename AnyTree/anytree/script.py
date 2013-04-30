f1b98be7e709c09ab073798c97577abcbc4d3ee8
Christophe Combelles <ccomb@free.fr>
2013-04-30 10:23:26 +0200

    Added a console script for migration

---
+++ AnyTree/anytree/script.py
@@ -0,0 +1,11 @@
+import argparse
+
+def main():
+    """ Main console script
+    """
+    raise NotImplementedError
+
+def migrate():
+    """ Migrate using other importing/mapping/processing modules
+    """
+    raise NotImplementedError
