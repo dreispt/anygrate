4afb8031eac60c0422b41b96df289d0cb400293d
Christophe Combelles <ccomb@free.fr>
2013-05-15 15:00:31 +0200

    Plug the data import

--- anygrate/importing.py
+++ anygrate/importing.py
@@ -1,13 +1,10 @@
 from os.path import basename
 
 
-def import_from_csv(filepath, connection):
+def import_from_csv(filepaths, connection):
     """ Import the csv file using postgresql COPY
     """
-    with connection.cursor() as cursor, open(filepath) as f:
-        copy = "COPY %s FROM STDOUT WITH CSV HEADER" % basename(filepath).rsplit('.', 1)[0]
-        # To test importing
-        #copy = "COPY %s FROM STDOUT WITH CSV HEADER" % basename(filepath).rsplit('.', 2)[0]
-        #print basename(filepath)
-        #print basename(filepath).rsplit('.', 2)[0]
-        cursor.copy_expert(copy, f)
+    for filepath in filepaths:
+        with connection.cursor() as cursor, open(filepath) as f:
+            copy = "COPY %s FROM STDOUT WITH CSV HEADER" % basename(filepath).rsplit('.', 1)[0]
+            cursor.copy_expert(copy, f)
