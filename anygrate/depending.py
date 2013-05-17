3d058e90c043f7d9cdb5406e31c53d193f248644
fjouatte <fjouatte@anybox.fr>
2013-05-17 16:55:12 +0200

    Add method get_sql_dependencies

--- anygrate/depending.py
+++ anygrate/depending.py
@@ -53,14 +53,141 @@ if __name__ == '__main__':
     main()
 
 
-def encapsulation_get_dep(username, pwd, dbname, models, excluded_models,
-                          path=None, seen=None, related_tables=None):
+def add_related_tables_to_tables_dependencies(target_connection, tables,
+                                              excluded_tables, path=None,
+                                              seen=None, related_tables=None):
+    res, related_tables = get_sql_dependencies(target_connection,
+                                               tables, excluded_tables)
+    for tbl in related_tables:
+        res.append(tbl)
+    return res
+
+
+def add_related_tables_to_models_dependencies(username, pwd, dbname, models,
+                                              excluded_models, path=None,
+                                              seen=None, related_tables=None):
 
     res, related_tables = get_dependencies(username, pwd, dbname, models,
                                            excluded_models)
     for tbl in related_tables:
         res.append(tbl)
     return res
+
+
+def get_sql_dependencies(target_connection, tables, excluded_tables,
+                         path=None, seen=None, related_tables=None):
+    """ Given a list of PSQL tables, return the full list of dependant tables,
+    ordered by dependencies. Warning are displayed if there are dependency loops
+    Set excluded_models to None if there is no table to exclude.
+    If you want to exclude some tables, use the following syntax :
+    excluded_tables = ['res.currency', 'res.country']
+    """
+    res = []
+    if seen is None:
+        seen = set()
+    if path is None:
+        path = ()
+    if excluded_tables is not None:
+        for excl_table in excluded_tables:
+            seen.add(excl_table)
+        excluded_tables = None
+    m2o = set()
+    m2m = set()
+    potentials_m2m = set()
+    related_tables = set()
+    for table in tables:
+        with target_connection.cursor() as c:
+            seen.add(table)
+            query_fk = """
+  SELECT pg_cl_2.relname as related_table
+  FROM pg_class pg_cl_1, pg_class pg_cl_2, pg_constraint,
+  pg_attribute pg_attr_1,
+  pg_attribute pg_attr_2 WHERE pg_cl_1.relname = '%s'
+  and pg_constraint.conrelid = pg_cl_1.oid
+  AND pg_cl_2.relkind = 'r' AND pg_cl_2.oid = pg_constraint.confrelid
+  AND pg_attr_1.attnum = pg_constraint.confkey[1]
+  AND pg_attr_1.attrelid = pg_cl_2.oid
+  AND pg_attr_2.attnum = pg_constraint.conkey[1]
+  AND pg_attr_2.attrelid = pg_cl_1.oid;
+  """ % table
+
+            query_ref = """
+  SELECT tc.table_name, kcu.column_name
+  FROM information_schema.table_constraints AS tc JOIN
+  information_schema.key_column_usage AS kcu ON
+  tc.constraint_name = kcu.constraint_name JOIN
+  information_schema.constraint_column_usage AS
+  ccu ON ccu.constraint_name = tc.constraint_name
+  WHERE constraint_type = 'FOREIGN KEY' AND
+  ccu.table_name='%s';""" % table
+            c.execute(query_ref)
+            ref = c.fetchall()
+
+            query_third_tables = """
+  SELECT DISTINCT ir_model_relation.name FROM ir_model, ir_model_relation
+  WHERE ir_model.model = '%s' AND ir_model.id = ir_model_relation.model;
+  """ % table.replace('_', '.')
+
+            c.execute(query_third_tables)
+            third_tables = c.fetchall()
+
+            for third_table in third_tables:
+                query_m2m = """
+  SELECT model FROM ir_model WHERE id IN
+  (SELECT ir_model_relation.model FROM ir_model_relation, ir_model
+  WHERE ir_model_relation.name = '%s'
+  AND ir_model_relation.model NOT IN
+  ( SELECT id FROM ir_model WHERE model = '%s'));
+  """ % (third_table[0], table)
+                c.execute(query_m2m)
+                potentials_m2m.add(c.fetchone()[0].replace('.', '_'))
+
+            c.execute(query_fk)
+            results_fk = c.fetchall()
+            if results_fk:
+                for fk in results_fk:
+                    # Cas des structures arborescentes (reflexives)
+                    tbl = fk[0]
+                    if tbl in path:
+                        LOG.warn('Dependency LOOP: '
+                                 '%s has a m2o to %s which is one of its ancestors (path=%r)',
+                                 table, tbl, path)
+                    if tbl not in seen:
+                        m2o.add(tbl)
+                        seen.add(tbl)
+            if third_tables:
+                for third_table in third_tables:
+                    tbl = third_table[0]
+                    if tbl not in seen:
+                        related_tables.add(tbl)
+                        seen.add(tbl)
+            if potentials_m2m:
+                for tbl in potentials_m2m:
+                    if tbl in path:
+                        LOG.warn('Dependency LOOP: '
+                                 '%s has a m2m to %s which is one of its ancestors (path=%r)',
+                                 table, tbl, path)
+                    if tbl not in seen:
+                        m2m.add(tbl)
+        for t in m2m:
+            results, related_tables = get_dependencies(target_connection, (t,),
+                                    path=path+(table,),
+                                    excluded_tables=excluded_tables,
+                                    seen=seen)
+            res += results
+        for t in m2o:
+            results, related_tables = get_dependencies(target_connection, (t,),
+                                    path=path+(table,),
+                                    excluded_tables=excluded_tables,
+                                    seen=seen)
+            res += results
+        #if model == 'ir.actions.actions':
+        #    model = 'ir.actions'
+        res.append(table)
+        if related_tables:
+            for table in related_tables:
+                res.append(table)
+    return res, related_tables
 
 
 def get_dependencies(username, pwd, dbname, models, excluded_models,
