039eb51648363b52e0eed8ab57b9bf9882777835
fjouatte ext:(%20%20on%20k6) <fjouatte@anybox.fr>
2013-05-17 18:45:02 +0200

    Try to implement the get_sql_dependencies part, find some ideas but not really a sucess

--- anygrate/migrating.py
+++ anygrate/migrating.py
@@ -8,6 +8,7 @@ from .importing import import_from_csv
 from .mapping import Mapping
 from .processing import CSVProcessor
 from .depending import add_related_tables_to_models_dependencies
+from .depending import add_related_tables_to_tables_dependencies
 from .depending import get_fk_to_update
 import logging
 from os.path import basename, join, abspath, dirname, exists
@@ -65,8 +66,8 @@ def main():
         shutil.rmtree(tempdir)
 
 
-def migrate(source_db, target_db, source_models, mapping_name, excluded_models=None,
-            target_dir=None):
+def migrate(source_db, target_db, source_models, mapping_name,
+            excluded_models=None, target_dir=None):
     """ The main migration function
     """
     source_connection = psycopg2.connect("dbname=%s" % source_db)
@@ -80,7 +81,7 @@ def migrate(source_db, target_db, source_models, mapping_name, excluded_models=N
     # we turn the list of wanted models into the full list of required models
     print(u'Computing the real list of models to export...')
     source_models = set(add_related_tables_to_models_dependencies('admin', 'admin',
-                        source_db, source_models, excluded_models))
+                                                   'test', source_models, excluded_models))
     print(u'The real list of models to export is: %s' % ', '.join(source_models))
 
     # compute the foreign keys to modify in the csv
