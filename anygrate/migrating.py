a92f656567b3a552ffcf78877863e6c2e13c6035
fjouatte ext:(%20%20on%20k6) <fjouatte@anybox.fr>
2013-05-17 15:58:14 +0200

    Typo ...

--- anygrate/migrating.py
+++ anygrate/migrating.py
@@ -81,7 +81,7 @@ def migrate(source_db, target_db, source_models, mapping_name, excluded_models=N
     # we turn the list of wanted models into the full list of required models
     print(u'Computing the real list of models to export...')
     source_models = set(encapsulation_get_dep('admin', 'admin',
-                        source_db, source_models, excluded_models)
+                        source_db, source_models, excluded_models))
     print(u'The real list of models to export is: %s' % ', '.join(source_models))
 
     # compute the foreign keys to modify in the csv
